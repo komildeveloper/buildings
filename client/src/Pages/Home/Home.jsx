3f156a37f85952d7b45a85ee796863dd0209a2da
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-06-30 16:21:31 +0500

    new updates again

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,11 +3,42 @@ import Navbar from '../../Components/Navbar'
 
 function Home() {
 	const [company, setCompany] = useState([])
+	const [complex, setComplex] = useState([])
+	const [complexAddress, setComplexAddress] = useState([])
+	const [rooms, setRooms] = useState(null)
+	const [banks, setBanks] = useState(null)
+	const [mortage, setMortage] = useState(null)
 
 	useEffect(() => {
 		fetch(`http://localhost:5000/company`)
 			.then(res => res.json())
 			.then(data => setCompany(data))
+	}, [])
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/complex`)
+			.then(res => res.json())
+			.then(data => setComplex(data))
+	}, [])
+	useEffect(() => {
+		fetch(`http://localhost:5000/complex-address`)
+			.then(res => res.json())
+			.then(data => setComplexAddress(data))
+	}, [])
+	useEffect(() => {
+		fetch(`http://localhost:5000/rooms`)
+			.then(res => res.json())
+			.then(data => setRooms(data))
+	}, [])
+	useEffect(() => {
+		fetch(`http://localhost:5000/banks`)
+			.then(res => res.json())
+			.then(data => setBanks(data))
+	}, [])
+	useEffect(() => {
+		fetch(`http://localhost:5000/mortage`)
+			.then(res => res.json())
+			.then(data => setMortage(data))
 	}, [])
 
 	return (
@@ -26,11 +57,17 @@ function Home() {
 							id='small'
 							className='block p-2 mb-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 						>
-							<option selected>Company</option>
-							<option defaultValue='US'>United States</option>
-							<option defaultValue='CA'>Canada</option>
-							<option defaultValue='FR'>France</option>
-							<option defaultValue='DE'>Germany</option>
+							<option selected hidden>
+								Company
+							</option>
+							{company &&
+								company.map((e, i) => {
+									return (
+										<option key={i} defaultValue={e.company_name}>
+											{e.company_name}
+										</option>
+									)
+								})}
 						</select>
 					</div>
 
@@ -45,11 +82,17 @@ function Home() {
 							id='small'
 							className='block p-2 mb-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 						>
-							<option selected>Complex</option>
-							<option defaultValue='US'>United States</option>
-							<option defaultValue='CA'>Canada</option>
-							<option defaultValue='FR'>France</option>
-							<option defaultValue='DE'>Germany</option>
+							<option selected hidden>
+								Complex
+							</option>
+							{complex &&
+								complex.map((e, i) => {
+									return (
+										<option key={i} defaultValue={e.complex_name}>
+											{e.complex_name}
+										</option>
+									)
+								})}
 						</select>
 					</div>
 
@@ -58,17 +101,23 @@ function Home() {
 							htmlFor='small'
 							className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900'
 						>
-							Room
+							Rooms
 						</label>
 						<select
 							id='small'
 							className='block p-2 mb-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 						>
-							<option selected>Room</option>
-							<option defaultValue='US'>United States</option>
-							<option defaultValue='CA'>Canada</option>
-							<option defaultValue='FR'>France</option>
-							<option defaultValue='DE'>Germany</option>
+							<option selected hidden>
+								Rooms
+							</option>
+							{rooms &&
+								rooms.map((e, i) => {
+									return (
+										<option key={i} defaultValue={e.room_number}>
+											{e.room_number}
+										</option>
+									)
+								})}
 						</select>
 					</div>
 
@@ -83,25 +132,40 @@ function Home() {
 							id='small'
 							className='block p-2 mb-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 						>
-							<option selected>Mortage</option>
-							<option defaultValue='US'>United States</option>
-							<option defaultValue='CA'>Canada</option>
-							<option defaultValue='FR'>France</option>
-							<option defaultValue='DE'>Germany</option>
+							<option selected hidden>Mortage</option>
+              {
+                mortage && mortage.map((e, i) => {
+                 return (
+							<option key={i}defaultValue={e.mortage_year}>{e.mortage_year}</option>
+                 )
+              })
+              }
 						</select>
 					</div>
 				</div>
-			</div>
 
-			{company &&
-				company.map((e, i) => {
-					return (
-						<div key={i} className='block bg-sky-300'>
-							<h3>{e.company_name}</h3>
-							<img alt={e.company_id} src={e.company_logo} />
-						</div>
-					)
-				})}
+				{company &&
+					company.map((e, i) => {
+						return (
+							<div
+								key={i}
+								className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'
+							>
+								<div className='flex justify-end px-4 pt-4'></div>
+								<div className='flex flex-col items-center pb-10'>
+									<img
+										className='mb-3 w-24 h-24 rounded-full shadow-lg'
+										src={e.company_logo}
+										alt='Bonnie image'
+									/>
+									<h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
+										{e.company_name}
+									</h5>
+								</div>
+							</div>
+						)
+					})}
+			</div>
 		</>
 	)
 }
--- server/src/modules/index.js
+++ server/src/modules/index.js
@@ -9,7 +9,7 @@ const Mortage = require('./mortage/mortage')
 router
 	.get('/company', Company.GET)
 	.get('/complex', Complex.GET)
-	.get('/complexAddress', ComplexAddress.GET)
+	.get('/complex-address', ComplexAddress.GET)
 	.get('/rooms', Rooms.GET)
 	.get('/banks', Banks.GET)
 	.get('/mortage', Mortage.GET)
